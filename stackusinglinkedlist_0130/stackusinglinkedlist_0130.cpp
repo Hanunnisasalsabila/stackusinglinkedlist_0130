884a1323be298f37eb1ba1b6bbd9ed1e2fa25c78
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2024-05-10 11:46:52 +0700

    Keluarkan elemen teratas dari tumpukan

--- stackusinglinkedlist_0130/stackusinglinkedlist_0130.cpp
+++ stackusinglinkedlist_0130/stackusinglinkedlist_0130.cpp
@@ -77,4 +77,8 @@ int main() {
             cin >> value;
             stack.push(value);
             break;
+        case 2:
+            if (!stack.isEmpty()) {
+                stack.pop();
+            }
 }
